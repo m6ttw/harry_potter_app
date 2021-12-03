97a304a5118059c515cc727fe5b0ed53803ebbd7
MW <matthewrhyswilliams@live.com>
2021-12-03 14:39:16 +0000

    GetById working in PupilController

--- backend/src/main/java/com/backend/controller/PupilController.java
+++ backend/src/main/java/com/backend/controller/PupilController.java
@@ -7,10 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/")
@@ -24,8 +27,13 @@ public class PupilController {
         return "Hello World!";
     }
 
-    @GetMapping(value = "/pupils")
+    @GetMapping("/pupils")
     public ResponseEntity<List<Pupil>> getAllPupils(){
         return new ResponseEntity<>(pupilRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/pupils/{id}")
+    public ResponseEntity getPupil(@PathVariable long id) {
+        return new ResponseEntity<>(pupilRepository.findById(id), HttpStatus.OK);
     }
 }
