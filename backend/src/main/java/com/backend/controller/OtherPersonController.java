6e53f059800f4c9f105a9b6c9d59529777cef989
m6ttw <matthewrhyswilliams@live.com>
2021-12-05 18:12:07 +0000

    POST method added for OtherPerson

--- backend/src/main/java/com/backend/controller/OtherPersonController.java
+++ backend/src/main/java/com/backend/controller/OtherPersonController.java
@@ -6,9 +6,7 @@ import com.backend.repository.OtherPersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,11 @@ public class OtherPersonController {
     @GetMapping(value = "/otherpersons")
     public ResponseEntity<List<OtherPerson>> getAllOtherPersons(){
         return new ResponseEntity<>(otherPersonRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/otherpersons")
+    public ResponseEntity<OtherPerson> createOtherPerson(@RequestBody OtherPerson otherPerson){
+        otherPersonRepository.save(otherPerson);
+        return new ResponseEntity<>(otherPerson, HttpStatus.CREATED);
     }
 }
