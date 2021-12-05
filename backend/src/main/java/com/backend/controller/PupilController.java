0a2632f659687656aafc83676492c7938398f627
m6ttw <matthewrhyswilliams@live.com>
2021-12-05 17:37:28 +0000

    POST method added for Pupil

--- backend/src/main/java/com/backend/controller/PupilController.java
+++ backend/src/main/java/com/backend/controller/PupilController.java
@@ -7,10 +7,7 @@ import com.backend.repository.PupilRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -36,5 +33,11 @@ public class PupilController {
     @GetMapping("/pupils/{id}")
     public ResponseEntity getPupil(@PathVariable long id){
         return new ResponseEntity<>(pupilRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/pupils")
+    public ResponseEntity<Pupil> createPupil(@RequestBody Pupil pupil){
+        pupilRepository.save(pupil);
+        return new ResponseEntity<>(pupil, HttpStatus.CREATED);
     }
 }
