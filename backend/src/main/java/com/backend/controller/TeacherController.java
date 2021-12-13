f99068e1bb6d90a622ed2ae60953b8c802f7fa02
m6ttw <matthewrhyswilliams@live.com>
2021-12-13 23:56:11 +0000

    changed controller route mapping

--- backend/src/main/java/com/backend/controller/TeacherController.java
+++ backend/src/main/java/com/backend/controller/TeacherController.java
@@ -12,29 +12,29 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/teachers/")
+@RequestMapping("/api")
 public class TeacherController {
 
     @Autowired
     private TeacherRepository teacherRepository;
 
-    @GetMapping("/")
+    @GetMapping("/teachers")
     public ResponseEntity<List<Teacher>> getAllTeachers(){
         return new ResponseEntity<>(teacherRepository.findAll(), HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/teachers/{id}")
     public ResponseEntity getTeacher(@PathVariable long id){
         return new ResponseEntity(teacherRepository.findById(id), HttpStatus.OK);
     }
 
-    @PostMapping("/")
+    @PostMapping("/teachers")
     public ResponseEntity<Teacher> createTeacher(@RequestBody Teacher teacher){
         teacherRepository.save(teacher);
         return new ResponseEntity<>(teacher, HttpStatus.CREATED);
     }
 
-    @PatchMapping(value = "/{id}/{firstName}")
+    @PatchMapping(value = "/teachers/{id}/{firstName}")
     public ResponseEntity<Teacher> updateTeacherPartially(@PathVariable Long id, @PathVariable String firstName){
         Teacher teacher = teacherRepository.findById(id).get();
         teacher.setFirstName(firstName);
